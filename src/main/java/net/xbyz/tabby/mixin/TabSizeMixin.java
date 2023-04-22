d74db6d06b62201b0417e0c9028ff7002281b8aa
Fruitloopins <fruitloopins@earthmc.net>
2023-04-23 06:38:21 +1000

    Initial commit

---
+++ src/main/java/net/xbyz/tabby/mixin/TabSizeMixin.java
@@ -0,0 +1,18 @@
+package net.xbyz.tabby.mixin;
+
+import net.minecraft.client.gui.hud.PlayerListHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(PlayerListHud.class)
+public class TabSizeMixin {
+	@ModifyConstant(constant = @Constant(longValue = 80L), method = "collectPlayerEntries")
+	private long modifyCount(long count) {
+		return 200L;
+	}
+	@ModifyConstant(constant = @Constant(intValue = 20), method = "render")
+	private int modifyMaxRows(int MAX_ROWS) {
+		return 40;
+	}
+}
