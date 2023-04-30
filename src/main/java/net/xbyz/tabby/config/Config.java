b03f4cb40526c5cf41df2d65a7a3179780f617a1
Fruitloopins <fruitloopins@earthmc.net>
2023-04-30 14:05:06 +1000

    Revert "1.19.3 port"
    This reverts commit f0fba16f0cbf99abdff83d1f73af30044ee34c3e.

--- src/main/java/net/xbyz/tabby/config/Config.java
+++ src/main/java/net/xbyz/tabby/config/Config.java
@@ -2,9 +2,9 @@ package net.xbyz.tabby.config;
 
 import io.wispforest.owo.config.annotation.Modmenu;
 
-@Modmenu(modId = "tabby")
+@Modmenu(modId = "tabby-1.19.3")
 @io.wispforest.owo.config.annotation.Config(name = "tabby-config", wrapperName = "TabbyConfig")
 public class Config {
-    public long maxCount = 200l;
+    public int maxCount = 200;
     public int maxRows = 40;
 }
