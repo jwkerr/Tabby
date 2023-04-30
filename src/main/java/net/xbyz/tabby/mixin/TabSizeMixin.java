5c3012bbe4834a180b8e2d9cc5561350168fa291
Fruitloopins <fruitloopins@earthmc.net>
2023-04-30 13:33:42 +1000

    1.19.3 port

--- src/main/java/net/xbyz/tabby/mixin/TabSizeMixin.java
+++ src/main/java/net/xbyz/tabby/mixin/TabSizeMixin.java
@@ -2,15 +2,16 @@ package net.xbyz.tabby.mixin;
 
 import net.minecraft.client.gui.hud.PlayerListHud;
 import net.xbyz.tabby.Tabby;
-import net.xbyz.tabby.config.Config;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(PlayerListHud.class)
 public class TabSizeMixin {
-	@ModifyConstant(constant = @Constant(longValue = 80L), method = "collectPlayerEntries")
-	private long modifyCount(long count) {
+	@ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Ljava/lang/Math;min(II)I", ordinal = 0), index = 1)
+	private int modifyCount(int count) {
 		return Tabby.CONFIG.maxCount();
 	}
 
