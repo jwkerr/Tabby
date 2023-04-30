f0fba16f0cbf99abdff83d1f73af30044ee34c3e
Fruitloopins <fruitloopins@earthmc.net>
2023-04-30 14:00:52 +1000

    1.19.3 port

--- src/main/java/net/xbyz/tabby/config/Config.java
+++ src/main/java/net/xbyz/tabby/config/Config.java
@@ -2,9 +2,9 @@ package net.xbyz.tabby.config;
 
 import io.wispforest.owo.config.annotation.Modmenu;
 
-@Modmenu(modId = "tabby-1.19.3")
+@Modmenu(modId = "tabby")
 @io.wispforest.owo.config.annotation.Config(name = "tabby-config", wrapperName = "TabbyConfig")
 public class Config {
-    public int maxCount = 200;
+    public long maxCount = 200l;
     public int maxRows = 40;
 }
--- src/main/java/net/xbyz/tabby/mixin/TabSizeMixin.java
+++ src/main/java/net/xbyz/tabby/mixin/TabSizeMixin.java
@@ -3,15 +3,13 @@ package net.xbyz.tabby.mixin;
 import net.minecraft.client.gui.hud.PlayerListHud;
 import net.xbyz.tabby.Tabby;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(PlayerListHud.class)
 public class TabSizeMixin {
-	@ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Ljava/lang/Math;min(II)I", ordinal = 0), index = 1)
-	private int modifyCount(int count) {
+	@ModifyConstant(constant = @Constant(longValue = 80L), method = "render")
+	private long modifyListLimit(long limit) {
 		return Tabby.CONFIG.maxCount();
 	}
 
