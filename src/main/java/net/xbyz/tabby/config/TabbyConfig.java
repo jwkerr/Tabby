b84e495851db55c3e0438a829d6ea7cde6592dfc
Fruitloopins <fruitloopins@earthmc.net>
2023-05-03 22:18:08 +1000

    Added adaptive tab feature

--- src/main/java/net/xbyz/tabby/config/TabbyConfig.java
+++ src/main/java/net/xbyz/tabby/config/TabbyConfig.java
@@ -5,8 +5,12 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 @me.shedaniel.autoconfig.annotation.Config(name = "Tabby")
 public class TabbyConfig implements ConfigData {
-    @Comment("The maximum players that can be rendered")
-    public long maxCount = 200l;
-    @Comment("The maximum rows that can be rendered")
+    @Comment("Set to true for the amount of rows to adapt based off how many players there are")
+    public boolean adaptive = true;
+    @Comment("The amount to divide the players online by to determine how many rows will be rendered when adaptive is set to true\nFormula: x / y = maxRows\nWhere x is the value of maxCount and y is the adaptiveDivisor value")
+    public int adaptiveDivisor = 5;
+    @Comment("The maximum players that can be rendered, set to 0 for the max to always be the current online player count")
+    public long maxCount = 0L;
+    @Comment("The maximum rows that can be rendered, this value is not used if adaptive is set to true")
     public int maxRows = 40;
 }
--- src/main/java/net/xbyz/tabby/mixin/TabSizeMixin.java
+++ src/main/java/net/xbyz/tabby/mixin/TabSizeMixin.java
@@ -1,23 +1,38 @@
 package net.xbyz.tabby.mixin;
 
 import me.shedaniel.autoconfig.AutoConfig;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.PlayerListHud;
 import net.xbyz.tabby.config.TabbyConfig;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(PlayerListHud.class)
 public class TabSizeMixin {
+	@Shadow @Final private MinecraftClient client;
+
 	@ModifyConstant(constant = @Constant(longValue = 80L), method = "collectPlayerEntries")
 	private long modifyCount(long count) {
 		TabbyConfig config = AutoConfig.getConfigHolder(TabbyConfig.class).getConfig();
+		if (config.maxCount == 0) {
+			return client.player.networkHandler.getListedPlayerListEntries().size();
+		}
 		return config.maxCount;
 	}
 
 	@ModifyConstant(constant = @Constant(intValue = 20), method = "render")
 	private int modifyMaxRows(int MAX_ROWS) {
 		TabbyConfig config = AutoConfig.getConfigHolder(TabbyConfig.class).getConfig();
+		if (config.adaptive) {
+			if (config.maxCount == 0) {
+				int onlinePlayers = client.player.networkHandler.getListedPlayerListEntries().size();
+				return onlinePlayers / config.adaptiveDivisor;
+			}
+			return (int) (config.maxCount / config.adaptiveDivisor);
+		}
 		return config.maxRows;
 	}
 }
