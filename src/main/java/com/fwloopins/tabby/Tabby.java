64d52445ff4130eb7b278949be3844888fac2bf9
Fruitloopins <49851457+Fruitloopins@users.noreply.github.com>
2023-08-09 06:30:02 +1000

    Remove comments in main

--- src/main/java/com/fwloopins/tabby/Tabby.java
+++ src/main/java/com/fwloopins/tabby/Tabby.java
@@ -8,16 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Tabby implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("Tabby");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 		AutoConfig.register(TabbyConfig.class, GsonConfigSerializer::new);
 
 		LOGGER.info("Tabby initialized");
